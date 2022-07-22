9205352851ac1b4698626789361e32beafb1b604
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-07-22 13:47:30 +0100

    list post set up

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-fetch('https://newhorizonathlone.ngo/wp-json/wp/v2/posts')
-  .then(response => response.json())
-  .then(data => console.log(data));
+async function getData() {
+  let url = "https://newhorizonathlone.ngo/wp-json/wp/v2/posts";
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function renderData() {
+  let data = await getData();
+  let posts = "";
+  posts += `<ul></ul>`;
+  data.forEach((post) => {
+    posts += `<li> Post id: ${post.id}</li>`;
+  });
+
+  let postList = document.querySelector("#app");
+  postList.innerHTML = posts;
+}
+
+renderData();
