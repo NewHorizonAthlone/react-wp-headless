67bb2ea8950e38e84ccfbdb8526e38427b702212
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-08-19 12:34:43 +0100

    Update PostsList.js

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,7 +7,10 @@ export const PostsList = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
 
+  console.log(posts);
+
   const postStatus = useSelector((state) => state.posts.status);
+  // const error = useSelector((state) => state.posts.error);
 
   useEffect(() => {
     if (postStatus === "idle") {
@@ -15,19 +18,21 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch]);
 
-  const renderedPosts = posts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  ));
+  const renderedPosts = () => {
+    return posts.map((post) => (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.slug}</h3>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    ));
+  };
 
   return (
     <section className="post-list">
-      <h2>Posts</h2>
+      <h2>Posts List</h2>
       {renderedPosts}
     </section>
   );
