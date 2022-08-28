6084ee782f19f2f2f4daec1d830c7a6e6089525d
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-08-28 13:49:36 +0100

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <h1>new horizon posts</h1>
+      <h1>New Horizon</h1>
       <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
-            <Link to="post">Post</Link>
           </li>
         </ul>
       </nav>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 const Layout = () => {
   return (
-    <main className="App">
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main className="App">
+        <Outlet />
+      </main>
+    </>
   );
 };
 
